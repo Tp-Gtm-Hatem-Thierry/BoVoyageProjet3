77d02049f27f589643ed362dd196e909a7a20daa
Thierry <x>
2018-09-20 11:15:32 +0200

    Regeneration de class model Destination car non visible dans la solution. (?)

--- BoVoyageProjet3/Models/Destination.cs
+++ BoVoyageProjet3/Models/Destination.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+
 namespace BoVoyageProjet3.Models
 {
     public class Destination
@@ -10,6 +11,6 @@ namespace BoVoyageProjet3.Models
         public int Id { get; set; }
         public string Continent { get; set; }
         public string Pays { get; set; }
-        public  string  description { get; set; }
+        public string description { get; set; }
     }
 }
