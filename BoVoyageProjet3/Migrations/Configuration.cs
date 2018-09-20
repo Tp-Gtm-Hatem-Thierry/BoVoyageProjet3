471fcf73c645161e4d9cffa75a50525967b04899
HatemSat <hatem.saturi@gmail.com>
2018-09-20 14:28:09 +0200

    Regeneration de la classe configuration

---
+++ BoVoyageProjet3/Migrations/Configuration.cs
@@ -0,0 +1,18 @@
+﻿using BoVoyageProjet3.Data;
+using System;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+
+namespace BoVoyageProjet3.Migrations
+{
+    
+    public class Configuration : DbMigrationsConfiguration<BoVoyageDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+        
+    }
+}
