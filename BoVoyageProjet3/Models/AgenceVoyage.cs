76261a794cd245b344ee2b8e73cb2d99f867f6b3
HatemSat <hatem.saturi@gmail.com>
2018-09-20 11:34:10 +0200

    Ajout Using dans les models

--- BoVoyageProjet3/Models/AgenceVoyage.cs
+++ BoVoyageProjet3/Models/AgenceVoyage.cs
@@ -2,13 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BoVoyageProjet3.Models
 {
     public class AgenceVoyage
     {
+
         public int Id { get; set; }
         public string Nom { get; set; }
         public ICollection<Voyage> Voyages { get; set; }
+        
     }
 }
--- BoVoyageProjet3/Models/Destination.cs
+++ BoVoyageProjet3/Models/Destination.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 
 namespace BoVoyageProjet3.Models
@@ -11,6 +13,6 @@ namespace BoVoyageProjet3.Models
         public int Id { get; set; }
         public string Continent { get; set; }
         public string Pays { get; set; }
-        public string description { get; set; }
+        public string Description { get; set; }
     }
 }
