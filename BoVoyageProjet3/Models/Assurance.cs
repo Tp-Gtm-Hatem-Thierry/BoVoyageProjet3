b9f0bc4e1ca7ac4ad036caac0b412203fd06effa
HatemSat <hatem.saturi@gmail.com>
2018-09-20 10:37:45 +0200

    Création des premiers models

---
+++ BoVoyageProjet3/Models/Assurance.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BoVoyageProjet3.Models
+{
+    public class Assurance
+    {
+        public int Id { get; set; }
+        public decimal Montant { get; set; }
+        public TypeAssurance TypeAssurance { get; set; }
+        //public virtual ICollection<DossierReservation> DossierReservations { get; set; }
+    }
+    public enum TypeAssurance { Annulation = 1 }
+
+}
---
+++ BoVoyageProjet3/Models/AssuranceDossierReservation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BoVoyageProjet3.Models
+{
+    public class AssuranceDossierReservation
+    {
+        [Key, Column(Order = 0)]
+        public int AssuranceId { get; set; }
+        [Key, Column(Order = 1)]
+        public int DossierReservationId { get; set; }
+
+        [ForeignKey("AssuranceId")]
+        public virtual Assurance Assurance { get; set; }
+
+        [ForeignKey("DossierReservationId")]
+        public virtual DossierReservation DossierReservation { get; set; }
+    }
+}
