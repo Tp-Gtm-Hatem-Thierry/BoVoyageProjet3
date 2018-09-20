e16e2ef171ce2839d5088b90cb9d1c50f5ac13ff
Eternal-Ben <Eternalst@live.fr>
2018-09-21 00:25:27 +0200

    Creation de la class configuration

---
+++ BoVoyageProjet3/Migrations/Configuration.cs
@@ -0,0 +1,21 @@
+﻿using BoVoyageProjet3.Data;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+
+namespace BoVoyageProjet3
+{
+    public class Configuration : DbMigrationsConfiguration<BoVoyageDbContext>
+    {
+        public Configuration() // le constructeur de la class Configuration
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+    }
+}
+/* Affichage>Autre fentre> add-migration "le nom de la migration" va creer une classe :
+  add-migration Init << nom de la migration que l'on a donné
+
+  update-database << creatin de la base de donnee*/
