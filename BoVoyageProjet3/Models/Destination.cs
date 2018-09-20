afbf1c8c25917d0ad388c1989b35e4b8c8280eb2
Thierry <x>
2018-09-20 11:53:14 +0200

    Ajout des attribues des tables Client et destination

--- BoVoyageProjet3/Models/Destination.cs
+++ BoVoyageProjet3/Models/Destination.cs
@@ -5,14 +5,21 @@ using System.Web;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-
 namespace BoVoyageProjet3.Models
 {
+//Model de table Destination
     public class Destination
     {
         public int Id { get; set; }
+
+        [Required]
         public string Continent { get; set; }
+
+        [Required]
+        [StringLength(100)]
         public string Pays { get; set; }
+
+        [StringLength(250)]
         public string Description { get; set; }
     }
 }
