6f9e739e1c59a276bbb9f67d07a385eab31e2aae
Thierry <x>
2018-09-20 12:09:16 +0200

    Creation des attribues du model de table Personne

--- BoVoyageProjet3/Models/Personne.cs
+++ BoVoyageProjet3/Models/Personne.cs
@@ -7,19 +7,35 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BoVoyageProjet3.Models
 {
+    //Model de tableau Personne avec ses attribues
     public abstract class Personne
     {
         public int Id { get; set; }
+
+        [StringLength(12)]
         public string Civilite { get; set; }
+
+        [Required]
+        [StringLength(50)]
         public string Nom { get; set; }
+
+        [Required]
+        [StringLength(50)]
         public string Prenom { get; set; }
+
+        [Required]
+        [StringLength(150)]
         public string Adresse { get; set; }
+
+        [Required]
+        [StringLength(12)]
         public string Telephone { get; set; }
+
+        [Required]
+        [StringLength(10)]
         public DateTime DateNaissance { get; set; }
-        public int Age
-        {
-            get { return DateTime.Today.Year - DateNaissance.Year; }
-        }
+
+        public int Age => DateTime.Today.Year - DateNaissance.Year;
 
     }
 }
