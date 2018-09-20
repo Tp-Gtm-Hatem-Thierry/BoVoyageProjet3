3e56c260377bbb01824d014f8c509a868d3b48a9
Thierry <x>
2018-09-20 14:19:04 +0200

    Creation Model de class : AssuracneDossierReservation ajout des attributs

---
+++ BoVoyageProjet3/Models/AssuranceDossierReservation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace BoVoyageProjet3.Models
+{
+    //Model des dossier de Reservation assurance
+    public class AssuranceDossierReservation
+    {
+        [Key, Column(Order = 0)]
+        public int AssuranceId { get; set; }
+        [Key, Column(Order = 1)]
+        public int DossierReservationId { get; set; }
+
+        [ForeignKey("AssuranceId")]
+        public virtual Assurance Assurance { get; set; }
+
+        [ForeignKey("DossierReservationId")]
+        public virtual DossierReservation DossierReservation { get; set; }
+    }
+}
