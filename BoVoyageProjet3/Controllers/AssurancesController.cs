b6170e703e9e619f9645e08e8d44bf549dd47bae
HatemSat <hatem.saturi@gmail.com>
2018-09-20 15:24:55 +0200

    Creation de 4controleurs

---
+++ BoVoyageProjet3/Controllers/AssurancesController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using BoVoyageProjet3.Data;
+using BoVoyageProjet3.Models;
+
+namespace BoVoyageProjet3.Controllers
+{
+    public class AssurancesController : ApiController
+    {
+        private BoVoyageDbContext db = new BoVoyageDbContext();
+
+        // GET: api/Assurances
+        public IQueryable<Assurance> GetAssurances()
+        {
+            return db.Assurances;
+        }
+
+        // GET: api/Assurances/5
+        [ResponseType(typeof(Assurance))]
+        public IHttpActionResult GetAssurance(int id)
+        {
+            Assurance assurance = db.Assurances.Find(id);
+            if (assurance == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(assurance);
+        }
+
+        // PUT: api/Assurances/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutAssurance(int id, Assurance assurance)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != assurance.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(assurance).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AssuranceExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Assurances
+        [ResponseType(typeof(Assurance))]
+        public IHttpActionResult PostAssurance(Assurance assurance)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Assurances.Add(assurance);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = assurance.Id }, assurance);
+        }
+
+        // DELETE: api/Assurances/5
+        [ResponseType(typeof(Assurance))]
+        public IHttpActionResult DeleteAssurance(int id)
+        {
+            Assurance assurance = db.Assurances.Find(id);
+            if (assurance == null)
+            {
+                return NotFound();
+            }
+
+            db.Assurances.Remove(assurance);
+            db.SaveChanges();
+
+            return Ok(assurance);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool AssuranceExists(int id)
+        {
+            return db.Assurances.Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ BoVoyageProjet3/Controllers/DestinationsController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using BoVoyageProjet3.Data;
+using BoVoyageProjet3.Models;
+
+namespace BoVoyageProjet3.Controllers
+{
+    public class DestinationsController : ApiController
+    {
+        private BoVoyageDbContext db = new BoVoyageDbContext();
+
+        // GET: api/Destinations
+        public IQueryable<Destination> GetDestinations()
+        {
+            return db.Destinations;
+        }
+
+        // GET: api/Destinations/5
+        [ResponseType(typeof(Destination))]
+        public IHttpActionResult GetDestination(int id)
+        {
+            Destination destination = db.Destinations.Find(id);
+            if (destination == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(destination);
+        }
+
+        // PUT: api/Destinations/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutDestination(int id, Destination destination)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != destination.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(destination).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DestinationExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Destinations
+        [ResponseType(typeof(Destination))]
+        public IHttpActionResult PostDestination(Destination destination)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Destinations.Add(destination);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = destination.Id }, destination);
+        }
+
+        // DELETE: api/Destinations/5
+        [ResponseType(typeof(Destination))]
+        public IHttpActionResult DeleteDestination(int id)
+        {
+            Destination destination = db.Destinations.Find(id);
+            if (destination == null)
+            {
+                return NotFound();
+            }
+
+            db.Destinations.Remove(destination);
+            db.SaveChanges();
+
+            return Ok(destination);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool DestinationExists(int id)
+        {
+            return db.Destinations.Count(e => e.Id == id) > 0;
+        }
+    }
+}
