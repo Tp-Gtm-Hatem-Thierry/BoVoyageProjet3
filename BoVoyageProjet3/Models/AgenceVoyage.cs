9a098cd4bff8d44ef3a934db5e8ba4ece35eaa01
Thierry <x>
2018-09-20 11:33:29 +0200

    Creation des annotations pour la base de donnée des class : AgenceVoyage Client

--- BoVoyageProjet3/Models/AgenceVoyage.cs
+++ BoVoyageProjet3/Models/AgenceVoyage.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -8,7 +10,14 @@ namespace BoVoyageProjet3.Models
     public class AgenceVoyage
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
         public string Nom { get; set; }
+
+        public int VoyageID { get; set; }
+
+        [ForeignKey("VoyageID")]
         public ICollection<Voyage> Voyages { get; set; }
     }
 }
--- BoVoyageProjet3/Models/Client.cs
+++ BoVoyageProjet3/Models/Client.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
-
 namespace BoVoyageProjet3.Models
 {
+//Model de table Personne pour la base de donnée
     public class Client : Personne
     {
+        [StringLength(50)]
         public string Email { get; set; }
     }
 }
